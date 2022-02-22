918b197daa2c8b3ccb2be66c993fb1aa654b557a
reed-chi <reed0017@hotmail.com>
2022-02-22 22:33:53 +0800

    Update logger_test.go
    修改依赖错误

--- lager/logger_test.go
+++ lager/logger_test.go
@@ -6,8 +6,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/lexkong/lager"
-	"github.com/lexkong/lager/lagertest"
+	"github.com/reed-chi/log/lager"
+	"github.com/reed-chi/log/lager/lagertest"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
